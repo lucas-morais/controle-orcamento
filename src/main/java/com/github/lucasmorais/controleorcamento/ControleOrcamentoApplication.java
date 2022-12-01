b840ca9ea0b01199c215c5f964075a898bd65eb3
Lucas Toledo <lucasmoraisss@gmail.com>
2022-12-01 09:33:07 -0300

    starting spring project

---
+++ src/main/java/com/github/lucasmorais/controleorcamento/ControleOrcamentoApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasmorais.controleorcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleOrcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleOrcamentoApplication.class, args);
+	}
+
+}
