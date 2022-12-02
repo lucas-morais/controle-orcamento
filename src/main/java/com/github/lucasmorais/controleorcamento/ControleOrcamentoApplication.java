440c1b8c77787f351e9267c5211927c0733b0030
Lucas Toledo <lucasmoraisss@gmail.com>
2022-12-02 08:14:28 -0300

    config: spring datasource configuration

--- src/main/java/com/github/lucasmorais/controleorcamento/ControleOrcamentoApplication.java
+++ src/main/java/com/github/lucasmorais/controleorcamento/ControleOrcamentoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControleOrcamentoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControleOrcamentoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ControleOrcamentoApplication.class, args);
+  }
 
 }
