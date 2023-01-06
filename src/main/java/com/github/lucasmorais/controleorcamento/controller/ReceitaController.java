3dfcc37c7d6cebddf01c4774a49afc84d24814ab
Lucas Toledo <lucasmoraisss@gmail.com>
2023-01-06 15:24:14 -0300

    feat: method to detail receita

--- src/main/java/com/github/lucasmorais/controleorcamento/controller/ReceitaController.java
+++ src/main/java/com/github/lucasmorais/controleorcamento/controller/ReceitaController.java
@@ -2,6 +2,7 @@ package com.github.lucasmorais.controleorcamento.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 import com.github.lucasmorais.controleorcamento.dto.receita.CriaReceitaDTO;
 import com.github.lucasmorais.controleorcamento.dto.receita.ListaReceitaDTO;
 import com.github.lucasmorais.controleorcamento.service.ReceitaService;
@@ -10,11 +11,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
@@ -25,6 +28,7 @@ public class ReceitaController {
   private ReceitaService service;
 
   @PostMapping
+  @Transactional
   public ResponseEntity<ListaReceitaDTO> criarReceita(@RequestBody @Valid CriaReceitaDTO receita) {
     ListaReceitaDTO receitaDTO = service.criar(receita);
     URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -38,5 +42,13 @@ public class ReceitaController {
     List<ListaReceitaDTO> receitas = this.service.listar();
     return ResponseEntity.ok(receitas);
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ListaReceitaDTO> detalhaReceita(@PathVariable("id") UUID id) {
+    ListaReceitaDTO receita = this.service.detalhar(id);
+    return ResponseEntity.ok(receita);
+  }
+
+
 
 }
