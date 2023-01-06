5253cfba3ef4eaf58042f2126c8b276e17265678
Lucas Toledo <lucasmoraisss@gmail.com>
2023-01-06 14:58:03 -0300

    refactor: validation now tests min value of variable 'valor'

--- src/main/java/com/github/lucasmorais/controleorcamento/dto/receita/CriaReceitaDTO.java
+++ src/main/java/com/github/lucasmorais/controleorcamento/dto/receita/CriaReceitaDTO.java
@@ -2,7 +2,11 @@ package com.github.lucasmorais.controleorcamento.dto.receita;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CriaReceitaDTO(String descricao, BigDecimal valor, LocalDate data) {
+public record CriaReceitaDTO(@NotBlank(message = "{descricao.obrigatorio}") String descricao,
+    @NotNull @DecimalMin(value = "0.1") BigDecimal valor, @NotNull LocalDate data) {
 
 }
