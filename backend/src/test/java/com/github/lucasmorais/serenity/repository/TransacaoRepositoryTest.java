0081b3707127266694314e825e3f67b7f035ecf3
lucas-morais <lucasmoraisss@gmail.com>
2023-06-20 13:40:52 -0300

    test(repository): transacoes nao podem ter mesas definicoes

--- backend/src/test/java/com/github/lucasmorais/serenity/repository/TransacaoRepositoryTest.java
+++ backend/src/test/java/com/github/lucasmorais/serenity/repository/TransacaoRepositoryTest.java
@@ -2,6 +2,8 @@ package com.github.lucasmorais.serenity.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.github.lucasmorais.serenity.builder.TransacaoBuilder;
+import com.github.lucasmorais.serenity.model.Despesa;
 import com.github.lucasmorais.serenity.model.Receita;
 import com.github.lucasmorais.serenity.model.Transacao;
 
@@ -33,5 +36,31 @@ public class TransacaoRepositoryTest {
         assertThat(receitaCriada.getValor()).isEqualTo(receita.getValor());
         assertThat(receitaCriada.getData()).isEqualTo(receita.getData());
         assertThat(receitaCriada.getDescricao()).isEqualTo(receita.getDescricao());
+    }
+
+    @Test
+    @DisplayName("Deveria retornar verdadeiro se já existe uma transação do mesmo tipo cadastrada na data")
+    public void retornaVerdadeiroSeTransacaoDoMesmoTipoComMesmaDataEDescricaoJaEstaCadastrada() {
+        
+        String descricao = "Receita Nova";
+        LocalDate data = LocalDate.parse("2023-12-01");
+        int mes = data.getMonth().getValue();
+        Receita receita = new TransacaoBuilder()
+                            .descricao(descricao)
+                            .data(data)
+                            .buildReceita();
+        
+
+
+        this.repository.save(receita);
+
+        boolean receitaJaExiste = this.repository
+            .existsByDescricaoAndDataMonthAndTipoTransacao(descricao, mes, Receita.class);
+        boolean despesaNaoExiste = this.repository
+            .existsByDescricaoAndDataMonthAndTipoTransacao(descricao, mes, Despesa.class);
+        
+        assertThat(receitaJaExiste).isTrue();
+        assertThat(despesaNaoExiste).isFalse();
+    
     }
 }
