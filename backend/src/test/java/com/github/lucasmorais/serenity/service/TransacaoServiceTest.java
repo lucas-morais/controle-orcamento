da1697e213f1f7294281df82292ad5d6440fed13
lucas-morais <lucasmoraisss@gmail.com>
2023-06-21 11:18:05 -0300

    fix(service): adiciona mock de validacao ao teste de cricao de transacao

--- backend/src/test/java/com/github/lucasmorais/serenity/service/TransacaoServiceTest.java
+++ backend/src/test/java/com/github/lucasmorais/serenity/service/TransacaoServiceTest.java
@@ -2,6 +2,8 @@ package com.github.lucasmorais.serenity.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
@@ -17,7 +19,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.github.lucasmorais.serenity.dto.CriaTransacaoDTO;
 import com.github.lucasmorais.serenity.dto.TransacaoDTO;
 import com.github.lucasmorais.serenity.model.Receita;
+import com.github.lucasmorais.serenity.model.TipoTransacao;
+import com.github.lucasmorais.serenity.model.Transacao;
 import com.github.lucasmorais.serenity.repository.TransacaoRepository;
+import com.github.lucasmorais.serenity.service.validation.TransacaoValidation;
 
 @ExtendWith(MockitoExtension.class)
 public class TransacaoServiceTest {
@@ -28,15 +33,25 @@ public class TransacaoServiceTest {
     @Mock
     private TransacaoRepository repository;
 
+    @Mock 
+    private TransacaoValidation validation;
+
+    private LocalDate dataPadrao = LocalDate.parse("2023-12-01");
+
     @Test
     @DisplayName("Deve acessar o repository, criar uma receita e retornar o DTO da receita criada")
     void criaReceitaComSucesso() {
-        CriaTransacaoDTO criaReceitaDto = new CriaTransacaoDTO(new BigDecimal("10.0"), "Nova receita", LocalDate.now());
-        Receita receitaCriada = new Receita(1L, new BigDecimal("10.00"), "Nova Receita", LocalDate.now());
-
+        CriaTransacaoDTO criaReceitaDto = new CriaTransacaoDTO(new BigDecimal("10.0"), "Nova receita", dataPadrao);
+        Receita receitaCriada = new Receita(1L, new BigDecimal("10.00"), "Nova Receita", dataPadrao);
+        TipoTransacao tipoTransacao = TipoTransacao.RECEITA;
+        
         when(repository.save(any(Receita.class))).thenReturn(receitaCriada);
 
-        TransacaoDTO receitaDto = service.criaTransacao(criaReceitaDto);
+        TransacaoDTO receitaDto = service.criaTransacao(criaReceitaDto, tipoTransacao);
+        
+        verify(this.validation).existsTransacaoDefinida(any(CriaTransacaoDTO.class), any());
+        verify(this.repository).save(any(Transacao.class));
+        
         assertThat(receitaDto).isExactlyInstanceOf(TransacaoDTO.class);
         assertThat(receitaDto.id()).isEqualTo(receitaCriada.getId());
         assertThat(receitaDto.valor()).isEqualTo(receitaCriada.getValor());
@@ -44,4 +59,24 @@ public class TransacaoServiceTest {
         assertThat(receitaDto.data()).isEqualTo(receitaCriada.getData());
 
     }
+
+    @DisplayName("Deve lançar exceção, criar uma receita e retornar o DTO da receita criada")
+    void lancaExcecaoAOTentarCriarReceitaJaDefinida() {
+        CriaTransacaoDTO criaReceitaDto = new CriaTransacaoDTO(new BigDecimal("10.0"), "Nova receita", dataPadrao);
+        Receita receitaCriada = new Receita(1L, new BigDecimal("10.00"), "Nova Receita", dataPadrao);
+        TipoTransacao tipoTransacao = TipoTransacao.RECEITA;
+        
+        when(repository.save(any(Receita.class))).thenReturn(receitaCriada);
+
+        TransacaoDTO receitaDto = service.criaTransacao(criaReceitaDto, tipoTransacao);
+
+        assertThat(receitaDto).isExactlyInstanceOf(TransacaoDTO.class);
+        assertThat(receitaDto.id()).isEqualTo(receitaCriada.getId());
+        assertThat(receitaDto.valor()).isEqualTo(receitaCriada.getValor());
+        assertThat(receitaDto.descricao()).isEqualTo(receitaCriada.getDescricao());
+        assertThat(receitaDto.data()).isEqualTo(receitaCriada.getData());
+
+    }
+
+    
 }
