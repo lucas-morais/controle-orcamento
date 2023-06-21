a879f3968dc03344a5000411b3dfdf518cd66333
lucas-morais <lucasmoraisss@gmail.com>
2023-06-21 11:13:53 -0300

    chore: remove imports não utilizados

--- backend/src/main/java/com/github/lucasmorais/serenity/repository/TransacaoRepository.java
+++ backend/src/main/java/com/github/lucasmorais/serenity/repository/TransacaoRepository.java
@@ -2,7 +2,6 @@ package com.github.lucasmorais.serenity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.github.lucasmorais.serenity.model.Transacao;
