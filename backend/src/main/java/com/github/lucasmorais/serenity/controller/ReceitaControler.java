c9eaa8da87e849db4a89d54a041e15b1ebbb2c84
lucas-morais <lucasmoraisss@gmail.com>
2023-06-22 08:32:29 -0300

    feat(controller): define metodo para criar receita

--- backend/src/main/java/com/github/lucasmorais/serenity/controller/ReceitaControler.java
+++ backend/src/main/java/com/github/lucasmorais/serenity/controller/ReceitaControler.java
@@ -1,10 +1,30 @@
 package com.github.lucasmorais.serenity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.lucasmorais.serenity.dto.CriaTransacaoDTO;
+import com.github.lucasmorais.serenity.dto.TransacaoDTO;
+import com.github.lucasmorais.serenity.model.TipoTransacao;
+import com.github.lucasmorais.serenity.service.TransacaoService;
 
 @RestController
 @RequestMapping("/receitas")
 public class ReceitaControler {
+
+    @Autowired
+    private TransacaoService service;
     
+    private final TipoTransacao tipoTransacao = TipoTransacao.RECEITA;
+
+    @PostMapping
+    public ResponseEntity<TransacaoDTO> craiReceita(@RequestBody CriaTransacaoDTO criaTransacaoDTO) {
+        TransacaoDTO receitaCriada = this.service.criaTransacao(criaTransacaoDTO, tipoTransacao);
+        return ResponseEntity.status(HttpStatus.CREATED).body(receitaCriada);
+    }
 }
--- backend/src/test/java/com/github/lucasmorais/serenity/controller/ReceitaControllerTest.java
+++ backend/src/test/java/com/github/lucasmorais/serenity/controller/ReceitaControllerTest.java
@@ -1,5 +1,12 @@
 package com.github.lucasmorais.serenity.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +15,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.github.lucasmorais.serenity.builder.TransacaoBuilder;
 import com.github.lucasmorais.serenity.dto.CriaTransacaoDTO;
 import com.github.lucasmorais.serenity.dto.TransacaoDTO;
+import com.github.lucasmorais.serenity.model.TipoTransacao;
 import com.github.lucasmorais.serenity.service.TransacaoService;
 
 @WebMvcTest(ReceitaControler.class)
@@ -19,21 +31,39 @@ import com.github.lucasmorais.serenity.service.TransacaoService;
 public class ReceitaControllerTest {
 
     @Autowired
-    private ReceitaControler receitaController;
-
+    private MockMvc mockMvc;
     
     @Autowired
-    private JacksonTester<CriaTransacaoDTO> criaReceitaDto;
+    private JacksonTester<CriaTransacaoDTO> criaReceitaJson;
 
     @Autowired
-    private JacksonTester<TransacaoDTO> receitaDto;
+    private JacksonTester<TransacaoDTO> receitaJson;
 
     @MockBean
     private TransacaoService service;
 
+    private LocalDate dataPadrao = LocalDate.parse("2023-12-01");
+
+    private TipoTransacao tipoTransacao = TipoTransacao.RECEITA;
+
+
     @Test
     @DisplayName("Cria receita com sucesso, retornando objeto JSON e Status Code 201")
-    void deveCriarReceita() {
+    void deveCriarReceita() throws IOException, Exception {
+        var criaReceitaDto = new TransacaoBuilder().data(dataPadrao).buildCriaTransacaoDto();
+        var receitaCriada = new TransacaoBuilder().id(1L).data(dataPadrao).buildTransacaoDTO();
 
+        when(this.service.criaTransacao(criaReceitaDto, tipoTransacao)).thenReturn(receitaCriada);
+
+        var response = mockMvc.perform(
+            post("/receitas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(criaReceitaJson.write(criaReceitaDto).getJson())
+            ).andReturn().getResponse();
+
+        var josnEsperado = receitaJson.write(receitaCriada).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(josnEsperado);
     }
 }
