ae76d536e24c572744bebb92edd14cd0bb02a602
lucas-morais <lucasmoraisss@gmail.com>
2023-06-23 09:10:48 -0300

    feat(controller): lista todas as receitas

--- backend/src/main/java/com/github/lucasmorais/serenity/controller/ReceitaControler.java
+++ backend/src/main/java/com/github/lucasmorais/serenity/controller/ReceitaControler.java
@@ -1,9 +1,11 @@
 package com.github.lucasmorais.serenity.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +27,18 @@ public class ReceitaControler {
     private final TipoTransacao tipoTransacao = TipoTransacao.RECEITA;
 
     @PostMapping
-    public ResponseEntity<TransacaoDTO> craiReceita(@RequestBody CriaTransacaoDTO criaTransacaoDTO) {
+    public ResponseEntity<TransacaoDTO> criaReceita(@RequestBody CriaTransacaoDTO criaTransacaoDTO) {
         TransacaoDTO receitaCriada = this.service.criaTransacao(criaTransacaoDTO, tipoTransacao);
         URI uri = UriComponentsBuilder
             .fromPath("/receitas/{id}")
             .buildAndExpand(receitaCriada.id())
             .toUri();
         return ResponseEntity.created(uri).body(receitaCriada);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransacaoDTO>> listaReceitas() {
+        List<TransacaoDTO> receitas = this.service.listaTransacoesPorTipo(tipoTransacao);
+        return ResponseEntity.ok(receitas);
     }
 }
