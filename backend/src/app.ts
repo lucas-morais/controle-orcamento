7353e4fa895f81f1c1e76a67a017481bd1685065
lucas-morais <lucasmoraisss@gmail.com>
2023-11-29 10:16:00 -0300

    feat: adiciona middleware json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors'
 
 const app = express();
 
-app.use(cors); 
+app.use(cors);
+app.use(json);
 
 export default app;
